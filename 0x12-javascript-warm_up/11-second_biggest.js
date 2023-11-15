0b43004433d5539edb58c06f6eabd53ee246853a
kidixx <imop0316@gmail.com>
2023-11-15 22:17:27 +0000

    done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const argList = process.argv;
-if (argList.length <= 3) {
-  console.log(0);
-} else {
-  console.log(argList.sort().reverse()[1]);
-}
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const str in arr) {
-  console.log(arr[str]);
-}
